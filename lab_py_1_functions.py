07bb55cddbec784dd514cfc6636e75b99a61d8e4
makelick <vi.neshcheret@gmail.com>
2022-02-10 04:00:07 +0200

    finish create_second_file

--- lab_py_1_functions.py
+++ lab_py_1_functions.py
@@ -17,3 +17,20 @@ def output_file(filename):
 
 
 def create_second_file(first_file_name, second_file_name):
+    with open(first_file_name, 'r') as infile:
+        with open(second_file_name, 'w') as outfile:
+            lines = infile.read().split("\n")
+            for i in lines:
+                if i != "":
+                    i.replace(";", " ")
+                    i.replace(",", " ")
+                    words = i.split(" ")
+                    outfile.write(str(count_same_words(words)) + " " + i + "\n")
+
+
+def count_same_words(words):
+    max_count = 0
+    for i in range(len(words)):
+        if words.count(words[i]) > max_count:
+            max_count = words.count(words[i])
+    return max_count
