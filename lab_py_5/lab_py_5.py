a8e2c2bd954c3d6edb0528345cadc2cfd7120f95
makelick <vi.neshcheret@gmail.com>
2022-05-01 00:45:26 +0300

    add main functions and fixes

--- lab_py_5/lab_py_5.py
+++ lab_py_5/lab_py_5.py
@@ -3,3 +3,6 @@ from BankAccount import *
 n = int(input("Enter the number of bank accounts: "))
 currents = []
 deposits = []
+init_arrays(n, currents, deposits)
+check_deposits(n, currents, deposits)
+print_accounts(n, currents, deposits)
