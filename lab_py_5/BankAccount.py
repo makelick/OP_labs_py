c607cb9f2909a6ab10db9785dbc58dce0f1cec58
makelick <vi.neshcheret@gmail.com>
2022-04-30 23:28:02 +0300

    add child classes of BankAccount

--- lab_py_5/BankAccount.py
+++ lab_py_5/BankAccount.py
@@ -1,14 +1,12 @@
 from abc import ABC, abstractmethod
 
-from setuptools.ssl_support import is_available
-
 
 class Date:
     year = 1900
     month = 1
     day = 1
 
-    def __init__(self, day, month, year):
+    def __init__(self, day=1, month=1, year=1900):
         self.day = day
         self.month = month
         self.year = year
@@ -61,9 +59,67 @@ class BankAccount(ABC):
         return self.is_available
 
     @abstractmethod
-    def add_balance(self):
+    def add_balance(self, a):
         pass
 
     @abstractmethod
-    def sub_balance(self):
+    def sub_balance(self, a):
         pass
+
+
+class CurrentAccount(BankAccount):
+    last_operation_date = Date()
+    balance = 0.0
+
+    def __init__(self, attributes):
+        super(CurrentAccount, self).__init__(attributes)
+        self.balance = attributes[2]
+        self.last_operation_date = Date()
+
+    def set_last_operation_date(self, date):
+        self.last_operation_date = date
+
+    def get_last_operation_date(self):
+        return self.last_operation_date
+
+    def get_balance(self):
+        return self.balance
+
+    def add_balance(self, a):
+        self.balance += a
+
+    def sub_balance(self, a):
+        self.balance -= a
+
+
+class DepositAccount(BankAccount):
+    opening_date = Date()
+    period = 0
+    balance = 0.0
+    rate = 0.0
+
+    def __init__(self, attributes):
+        super(DepositAccount, self).__init__(attributes)
+        self.balance = attributes[2]
+        self.rate = attributes[3]
+        att_for_date = attributes[4].split('.')
+        self.opening_date = Date(att_for_date[0], att_for_date[1], att_for_date[2])
+        self.period = attributes[5]
+
+    def get_opening_date(self):
+        return self.opening_date
+
+    def get_period(self):
+        return self.period
+
+    def get_balance(self):
+        return self.balance
+
+    def get_rate(self):
+        return self.rate
+
+    def add_balance(self, a):
+        self.balance += a
+
+    def sub_balance(self, a):
+        self.balance -= a
